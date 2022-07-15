c347d6d789f45c271ec6aa30a1d5145b8e807750
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2022-07-15 15:13:08 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,13 +2,19 @@ from genetics import *
 import statistics
 
 if __name__ == '__main__':
+    # Teste
+    # solucao = "10000000000000000000001000000000000000000000"
+    # print(solution_fitness(solucao))
+    # print(solution_to_humans(solucao))
+
+    # exit(0)
     # Gera população inicial
     population = [generate_solution() for _ in range(0, 40)]
 
-    for i in range(0, 10000):
+    for i in range(0, 1000):
         print('Geração #%d: [MÉDIA: %f]' % (i, statistics.mean([solution_fitness(x) for x in population])))
-        print('\t', end='')
-        print([solution_to_humans(x) for x in population])
-        print()
-        print()
-        population = generate_next_population(population, 10)
+        # print('\t', end='')
+        # print([solution_to_humans(x) for x in population])
+        # print()
+        # print()
+        population = generate_next_population(population, 40)
