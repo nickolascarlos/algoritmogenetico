1cb4c675ff0fb85f46d5588a0b7bc7537144734e
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2022-07-15 15:12:50 -0300

    Update genetics.py

--- genetics.py
+++ genetics.py
@@ -5,18 +5,30 @@ def F6(x, y):
     return 0.5 - (sin(sqrt(x**2 + y**2))**2 - 0.5)/(1 + 0.001*(x**2 + y**2))**2
 
 def solution_fitness(solution):
-    x = int(solution[0:20], 2) * (200/(2**22 - 1))
-    y = int(solution[20:40], 2) * (200/(2**22 - 1))
+    x = int(solution[0:22], 2)
+    x *= 200/(2**22 - 1)
+    x -= 100
+
+    y = int(solution[22:44], 2)
+    y *= 200/(2**22 - 1)
+    y -= 100
+
     return F6(x, y)
 
 def solution_to_humans(solution):
-    x = int(solution[0:20], 2) * (200/(2**22 - 1))
-    y = int(solution[20:40], 2) * (200/(2**22 - 1))
+    x = int(solution[0:22], 2) 
+    x *= 200/(2**22 - 1)
+    x -= 100
+
+    y = int(solution[22:44], 2)
+    y *= 200/(2**22 - 1)
+    y -= 100
+
     return (x, y)
 
 def generate_solution():
     bits = ''
-    for _ in range(0, 40):
+    for _ in range(0, 44):
         bits += '0' if random.random() > 0.5 else '1'
     return bits
 
@@ -33,7 +45,7 @@ def roll_roullete(population):
     roullete_association = generate_roullete_association(population)
     pivot = random.uniform(0, max([y for _, y in roullete_association]))
     for s, a in roullete_association:
-        if pivot < a:
+        if  pivot < a:
             return s
 
 def mutate(solution, factor):
@@ -41,14 +53,21 @@ def mutate(solution, factor):
 
 def reproduce(father, mother):
     # Crossover
-    child1 = father[0:20] + mother[20:40]
-    child2 = mother[0:20] + father[20:40]
+    child1 = father[0:22] + mother[22:44]
+    child2 = mother[0:22] + father[22:44]
 
     # Mutação
-    child1 = mutate(child1, 0.1)
-    child2 = mutate(child2, 0.1)
+    child1 = mutate(child1, 0.0002)
+    child2 = mutate(child2, 0.0002)
 
     return child1, child2
+
+def best_solution(population):
+    bs = population[0]
+    for i in range(1, len(population)):
+        if solution_fitness(population[i]) > solution_fitness(bs):
+            bs = population[i]
+    return bs
 
 def generate_next_population(population, population_size):
     new_population = []
@@ -58,4 +77,7 @@ def generate_next_population(population, population_size):
         c1, c2 = reproduce(father, mother)
         new_population.append(c1)
         new_population.append(c2)
+
+    new_population[random.randint(0, len(new_population) - 1)] = best_solution(population)
+
     return new_population
